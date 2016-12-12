53137f23ccb820ac32dc0bde7bc0c66131148aff
KerryAlsace <kerrydougherty@gmail.com>
2016-12-12 17:27:50 -0500

    add navbar and start building out js files for other models

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -15,6 +15,30 @@
           templateUrl: 'games/games.html',
           controller: 'GamesController as vm'
         })
+        .state('home.newGame', {
+          url: 'games/new',
+          templateUrl: 'games/new.html',
+          controller: 'GamesController as vm'
+        })
+        .state('home.showGame', {
+          url: 'games/:id',
+          templateUrl: 'games/show.html',
+          controller: 'GamesController as vm'
+        })
+        .state('home.publishers', {
+          url: 'publishers',
+          templateUrl: 'publishers/publishers.html',
+          controller: 'PublishersController as vm'
+        })
+        .state('home.genres', {
+          url: 'genres',
+          templateUrl: 'genres/genres.html',
+          controller: 'GenresController as vm'
+        }).state('home.platforms', {
+          url: 'platforms',
+          templateUrl: 'platforms/platforms.html',
+          controller: 'PlatformsController as vm'
+        })
 
       $urlRouterProvider.otherwise('/')
     })
