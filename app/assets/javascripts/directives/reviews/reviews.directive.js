58c0b78b576528347db3d771a88d6a029ad1bc6d
KerryAlsace <kerrydougherty@gmail.com>
2017-01-09 20:03:47 -0500

    get reviews working correctly

--- app/assets/javascripts/directives/reviews/reviews.directive.js
+++ app/assets/javascripts/directives/reviews/reviews.directive.js
@@ -1,43 +1,13 @@
 (function() {
   'use strict';
 
-  // function ReviewsController() {
-  //   var vm = this;
-  //
-  //   debugger
-  //   // //
-  //   // // vm.reviews = reviews;
-  //   //
-  //   // activate();
-  //   //
-  //   // function activate() {
-  //   //   debugger
-  //   //   // console.log(vm.reviews);
-  //   // }
-  //   //
-  //   // // vm.reviews = [];
-  //   //
-  //   // // vm.addReview = addReview;
-  //   //
-  //   // // function addReview(review) {
-  //   // //   vm.reviews.push(review);
-  //   // // }
-  // }
-  //
-  // angular
-  //   .module('app')
-  //   .controller('ReviewsController', ReviewsController);
-
   function Reviews() {
     return {
       templateUrl: 'directives/reviews/reviews.html',
       scope: {
         reviews: '='
-        // content: '@',
-        // goodfors: '@'
       },
       transclude: true,
-      // controller: 'GameController as vm',
       restrict: 'E'
     };
   }
