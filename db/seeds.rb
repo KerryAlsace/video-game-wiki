7605e832dd7b72cff9b3a8a5bccf15f2133bd147
KerryAlsace <kerrydougherty@gmail.com>
2017-01-13 13:21:41 -0500

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,22 +27,110 @@ require 'faker'
 #   r = Review.create(content: "Game Review #{id}", game: gm)
 # end
 
+# 5.times do |i|
+#   id = i + 1
+#   pb = Publisher.create(name: "#{Faker::Cat.registry}")
+#   g = Genre.create(name: "#{Faker::Book.unique.genre}")
+#   pl = Platform.create(name: "#{Faker::Ancient.unique.primordial}")
+#   gm = Game.create(title: "#{Faker::App.unique.name}", description: "#{Faker::Lorem.paragraph}", publisher: pb, genre: g)
+#   pg = PlatformGame.create(game: gm, platform: pl)
+#   r = Review.create(content: "#{Faker::TwinPeaks.quote}", game: gm)
+#   gf = Goodfor.create(activity: "#{Faker::Superhero.power}")
+# end
+#
+# 5.times do |i|
+#   n = i + 1
+#   Goodfor.find(n).reviews << Review.find(n)
+#   if n - 1 > 0 && n + 1 < 6
+#     r = n + 1
+#     Goodfor.find(r).reviews << Review.find(r)
+#   end
+# end
+
+Platform.create(name: "Playstation 4")
+Platform.create(name: "Playstation 3")
+Platform.create(name: "Playstation VR")
+Platform.create(name: "Wii U")
+Platform.create(name: "Nintendo Switch")
+Platform.create(name: "Nintendo 3DS")
+Platform.create(name: "XBox 1")
+Platform.create(name: "XBox 360")
+Platform.create(name: "PS Vita")
+Platform.create(name: "PC")
+
+Publisher.create(name: "2K Games")
+Publisher.create(name: "CD Projekt RED")
+Publisher.create(name: "Activision")
+Publisher.create(name: "Blizzard")
+Publisher.create(name: "Rockstar Games")
+Publisher.create(name: "Atari")
+Publisher.create(name: "Bandai")
+Publisher.create(name: "Capcom")
+Publisher.create(name: "EA")
+Publisher.create(name: "Disney")
+Publisher.create(name: "Konami")
+Publisher.create(name: "Microsoft Games Studio")
+Publisher.create(name: "Nintendo")
+Publisher.create(name: "Riot Games")
+Publisher.create(name: "TellTale Games")
+Publisher.create(name: "Ubisoft")
+Publisher.create(name: "Valve")
+Publisher.create(name: "Sony")
+
+Genre.create(name: "action")
+Genre.create(name: "adventure")
+Genre.create(name: "fighting games")
+Genre.create(name: "first-person shooters")
+Genre.create(name: "flight/flying")
+Genre.create(name: "party")
+Genre.create(name: "platformer")
+Genre.create(name: "puzzle")
+Genre.create(name: "racing")
+Genre.create(name: "real-time strategy")
+Genre.create(name: "role-playing")
+Genre.create(name: "simulation")
+Genre.create(name: "sports")
+Genre.create(name: "strategy")
+Genre.create(name: "third-person shooter")
+Genre.create(name: "turn-based strategy")
+Genre.create(name: "wargames")
+Genre.create(name: "wrestling")
+
+Goodfor.create(activity: "playing solo")
+Goodfor.create(activity: "playing with friend(s)")
+Goodfor.create(activity: "cosplay")
+Goodfor.create(activity: "relaxing")
+Goodfor.create(activity: "learning")
+Goodfor.create(activity: "newbs")
+Goodfor.create(activity: "getting amped up")
+Goodfor.create(activity: "playing while drunk")
+Goodfor.create(activity: "your adventurer streak")
+Goodfor.create(activity: "your Krombopolis Michael streak")
+Goodfor.create(activity: "your Bob the Builder streak")
+Goodfor.create(activity: "playing to remember (retro)")
+Goodfor.create(activity: "playing to forget")
+Goodfor.create(activity: "making new friends")
+Goodfor.create(activity: "making new enemies")
+
+
+g = Game.create(title: "The Witcher 3", description: "A Witcher Game", publisher_id: 2, genre_id: 2)
+PlatformGame.create(game_id: 1, platform_id: 1)
+PlatformGame.create(game_id: 1, platform_id: 7)
+PlatformGame.create(game_id: 1, platform_id: 10)
 5.times do |i|
   id = i + 1
-  pb = Publisher.create(name: "#{Faker::Cat.registry}")
-  g = Genre.create(name: "#{Faker::Book.unique.genre}")
-  pl = Platform.create(name: "#{Faker::Ancient.unique.primordial}")
-  gm = Game.create(title: "#{Faker::App.unique.name}", description: "#{Faker::Lorem.paragraph}", publisher: pb, genre: g)
-  pg = PlatformGame.create(game: gm, platform: pl)
-  r = Review.create(content: "#{Faker::TwinPeaks.quote}", game: gm)
-  gf = Goodfor.create(activity: "#{Faker::Superhero.power}")
+  r = Review.create(content: "#{Faker::StarWars.unique.quote}", game_id: 1)
+  Goodfor.find(id).reviews << r
 end
 
-5.times do |i|
-  n = i + 1
-  Goodfor.find(n).reviews << Review.find(n)
-  if n - 1 > 0 && n + 1 < 6
-    r = n + 1
-    Goodfor.find(r).reviews << Review.find(r)
+15.times do |n|
+  g = Game.create(title: "#{Faker::App.unique.name}", description: "#{Faker::Lorem.paragraph}", publisher_id: rand(18), genre_id: rand(18))
+  PlatformGame.create(game_id: g.id, platform_id: 1)
+  PlatformGame.create(game_id: g.id, platform_id: 7)
+  PlatformGame.create(game_id: g.id, platform_id: 10)
+  5.times do |i|
+    id = i + 1
+    r = Review.create(content: "#{Faker::StarWars.quote}", game_id: g.id)
+    Goodfor.find(id).reviews << r
   end
 end
