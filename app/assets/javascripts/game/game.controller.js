687b2cf725ac9d944c73ec20cb5bb1a849017038
KerryAlsace <kerrydougherty@gmail.com>
2017-01-13 16:58:17 -0500

    new reviews are correctly added and review form is emptied upon submit:

--- app/assets/javascripts/game/game.controller.js
+++ app/assets/javascripts/game/game.controller.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  function GameController(game, GamesFactory) {
+  function GameController(game, GamesFactory, $filter) {
     var vm = this;
 
     vm.game = game.data;
@@ -36,12 +36,21 @@
       vm.checkedGoodfors.forEach(function(goodfor_id) {
         vm.review.goodfor_ids.push(goodfor_id)
       })
+
       return GamesFactory.createReview(vm.review, vm.game.id)
                         .then(addReview)
     }
 
     function addReview(data) {
       vm.game.reviews.push(data);
+      getReviews();
+      vm.review = {};
+      vm.checkedGoodfors = [];
+      angular.forEach(vm.goodfors, function(goodfor) {
+        goodfor.selected = false;
+      })
+      vm.form.$setPristine();
+      vm.form.$setUntouched();
       return vm.reviews.push(data);
     }
 
